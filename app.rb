357563f5c73a5d0619231077bc58558e3fc6e195
Tomoya Tabuchi <t@tomoyat1.com>
2016-05-14 23:18:04 +0900

    Basic GET /v1/task

--- app.rb
+++ app.rb
@@ -1,4 +1,24 @@
 require 'sinatra'
 require 'mongo'
+require 'json'
 
-require_relative 'models/init'
+
+
+before do
+  @client = Mongo::Client.new([ 'localhost:27017' ], :database => 'homequest')#, :user => 'root', :password => 'abc123')
+end
+
+# GET /v1/task : すべてのクエストを取る
+
+get '/v1/task' do
+  @tasks = Array.new
+  content_type :json
+  @client[:tasks].find.each do |document|
+    @tasks << document
+  end
+end
+
+post '/v1/task' do
+
+end
+
