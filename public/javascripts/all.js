331fab00289e47ccfb43b91bd314ed3ba9896250
Tamrin007 <tamrin.bitterchococo@gmail.com>
2016-05-15 10:33:01 +0900

    implement function star()

--- public/javascripts/all.js
+++ public/javascripts/all.js
@@ -24,3 +24,28 @@ window.loadHomeQuestToken = loadHomeQuestToken;
 window.saveHomeQuestToken = saveHomeQuestToken;
 window.redirect = redirect;
 window.HomeQuest_API = HomeQuest_API;
+
+function star(level) {
+    var out;
+    switch (level) {
+        case 1:
+            out = "★☆☆☆☆：チョ～かんたん！";
+            break;
+        case 2:
+            out = "★★☆☆☆：かんたん！";
+            break;
+        case 3:
+            out = "★★★☆☆：ふつう";
+            break;
+        case 4:
+            out = "★★★★☆：時間がかかるかも？";
+            break;
+        case 5:
+            out = "★★★★★：めっちゃムズい！";
+            break;
+        default:
+            out = "ERROR!!"
+    }
+
+    return out;
+}
